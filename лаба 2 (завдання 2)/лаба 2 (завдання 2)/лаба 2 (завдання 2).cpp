6013842c6a9a1b1a5be8021d0eac454be4c031ea
YanaPonomaryova <148452518+YanaPonomaryova@users.noreply.github.com>
2025-04-15 20:29:00 +0300

    Add files via upload

---
+++ лаба 2 (завдання 2)/лаба 2 (завдання 2)/лаба 2 (завдання 2).cpp
@@ -0,0 +1,7 @@
+﻿#include <stdio.h>
+
+int main() {
+    const float PI = 3.14;
+    printf("value PI: %.2f\n", PI);
+    return 0;
+}
---
+++ лаба 2 (завдання 3)/лаба 2 (завдання 3)/лаба 2 (завдання 3).cpp
@@ -0,0 +1,7 @@
+﻿#include <stdio.h>
+
+int main() {
+    char symbol = 'A';
+    printf("symbol: %c, ASCII-code: %d\n", symbol, symbol);
+    return 0;
+}
